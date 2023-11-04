efed7f9ea3f681a2ea16aea0c6278d770018ffa2
Marceli Janczy <marcel2257@gmail.com>
2023-11-04 21:23:05 +0100

    Revert "Camera follow player"
    This reverts commit ae309a5c1c7a7c3f88de08851a7f71419097361b.

--- Assets/Scripts/CameraFollowPlayer.cs
+++ Assets/Scripts/CameraFollowPlayer.cs
@@ -4,21 +4,15 @@ using UnityEngine;
 
 public class CameraFollowPlayer : MonoBehaviour
 {
-    public Transform target;
-    public Vector3 offset;
-    [Range(1,10)]
-    public float smoothFactor;
-
-    private void FixedUpdate()
+    // Start is called before the first frame update
+    void Start()
     {
-        Follow();
+        
     }
 
-    void Follow()
+    // Update is called once per frame
+    void Update()
     {
-        Vector3 targetPosition = target.position + offset;
-        Vector3 smoothPosition = Vector3.Lerp(transform.position, target.position, smoothFactor*Time.fixedDeltaTime);
-        transform.position = smoothPosition;
+        
     }
-
 }
