8cc565b6bc77b820aa8f9778862392073c4e5b66
Marceli Janczy <marcel2257@gmail.com>
2023-11-30 21:47:54 +0100

    no

--- Assets/Scripts/CameraFollowPlayer.cs
+++ Assets/Scripts/CameraFollowPlayer.cs
@@ -8,6 +8,7 @@ public class CameraFollowPlayer : MonoBehaviour
     public Vector3 offset;
     [Range(1,10)]
     public float smoothFactor;
+    public Vector3 minValues, maxValues;
 
     private void FixedUpdate()
     {
@@ -17,9 +18,14 @@ public class CameraFollowPlayer : MonoBehaviour
     void Follow()
     {
         Vector3 targetPosition = target.position + offset;
-        Vector3 smoothPosition = Vector3.Lerp(transform.position, targetPosition, smoothFactor*Time.fixedDeltaTime);
+        Vector3 boundPosition = new Vector3(
+            Mathf.Clamp(targetPosition.x, minValues.x,maxValues.x),
+            Mathf.Clamp(targetPosition.y, minValues.y, maxValues.y),
+            Mathf.Clamp(targetPosition.z, minValues.z, maxValues.z)
+        );
+
+        Vector3 smoothPosition = Vector3.Lerp(transform.position, boundPosition, smoothFactor*Time.fixedDeltaTime);
         smoothPosition.z = transform.position.z;
-        
         transform.position = smoothPosition;
     }
 
